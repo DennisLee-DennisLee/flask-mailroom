06ea2e020713a7ae079a8b6853938bc7c0c78afc
Dennis Lee <dennis271lee@yahoo.com>
2019-09-29 02:21:48 -0700

    Implemented the create donation page and updated the base.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -16,6 +16,19 @@ def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
     
+@app.route('/create/', methods=['GET', 'POST'])
+# @app.route('/create/')
+def donate():
+    if request.method == 'GET':
+        return render_template('create.jinja2')
+
+    if request.method == 'POST':
+        amount, donor = int(request.form['amount']), request.form['donor']
+        Donation.create(
+            value=request.form['amount'],
+            donor=Donor.get(name=request.form['donor'])
+        )
+        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
